4ac1eb446af287a7b3ec08e6fe5064e548badfc6
Crea <77243990+CreaTikaa@users.noreply.github.com>
2024-12-16 18:30:24 +0100

    Update yt-next-gen.sh

--- tp2/yt-next-gen.sh
+++ tp2/yt-next-gen.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-while read LINE
-do 
-video_title=$(yt-dlp --quiet --no-warnings --get-title "$LINE")
-mkdir "/opt/yt/downloads/$video_title/"
-yt-dlp --quiet --no-warnings -o "/opt/yt/downloads/$video_title/%(title)s.mp4" "$LINE"
-yt-dlp --quiet --no-warnings --get-description $LINE >> "/opt/yt/downloads/$video_title/description"
+while true
+do
+  LINE=$(head -n 1 "/opt/yt/urls.txt")
+  if [[ ! $LINE =~ ^https://www\.youtube\.com/watch\?v=([a-z]|[A-Z]|[0-9]|'_'|'-'){11}$ ]]; then
+    sed -i '1d' "/opt/yt/urls.txt"
+  else
+     if [ ! -f "/var/log/yt/download.log" ]; then
+       echo "Pas de fichier logs, vérifiez /var/log/yt/downloads.log"
+       exit 1
+     fi
 
-echo "Video : $LINE was downloaded"
-echo "File Path : /opt/yt/downloads/$video_title/$video_title.mp4"
+     VIDEO_TITLE=$(yt-dlp --quiet --no-warnings --get-title $LINE)
+     VIDEO_TITLE="${VIDEO_TITLE// /_}"
 
-date=$(date '+%y-%m-%d %T')
-echo "[$date] Video $LINE was downloaded. File path : /opt/yt/downloads/$video_title/$video_title.mp4" >> "/var/log/yt/download.log"
-done < /opt/yt/urls.txt
+     mkdir /opt/yt/downloads/${VIDEO_TITLE}
+     yt-dlp --quiet --no-warnings --get-description "$LINE" >> /opt/yt/downloads/${VIDEO_TITLE}/description
+     yt-dlp -f mp4 --quiet --no-warnings -o "/opt/yt/downloads/${VIDEO_TITLE}/${VIDEO_TITLE}.mp4" "$LINE"
 
+     echo "Video : $LINE was downloaded."
+     echo "File path: /opt/yt/downloads/$VIDEO_TITLE/$VIDEO_TITLE.mp4"
 
-
+    date=$(date '+%y-%m-%d %T')
+    echo "[$date] Video $LINE was downloaded. File path : /opt/yt/downloads/$video_title/$video_title.mp4" >> "/var/log/yt/download.log"
+    sed -i '1d' "/opt/yt/urls.txt"
+  fi
+done < urls.txt
+exit
